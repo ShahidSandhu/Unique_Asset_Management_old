673d6b6e0baedb2d84847733f2dcb5e76de0556b
Shahid Sandhu <sandhulive@gmail.com>
2024-10-24 22:58:12 +0500

    just react project added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Add this at the top of the middleware
+    
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -142,3 +145,4 @@ REST_FRAMEWORK = {
     ],
 }
 
+CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins for now, you can limit this later
